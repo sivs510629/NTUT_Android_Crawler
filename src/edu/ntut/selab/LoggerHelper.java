d43e8b1e8528c168ff7115ab38e2b810d6296fad
Roger Huang <sivs510629@gmail.com>
2015-07-14 17:46:37 +0800

    Initial Commit

--- src/edu/ntut/selab/LoggerHelper.java
+++
@@ -1,9 +0,0 @@
-package edu.ntut.selab;
-
-import java.util.logging.Logger;
-
-public class LoggerHelper {
-	public static void warning(String msg) {
-		Logger.getLogger("log").warning(msg);
-	}
-}
--- src/edu/ntut/selab/event/SwipeType.java
+++
@@ -1,6 +0,0 @@
-package edu.ntut.selab.event;
-
-public class SwipeType {
-	public final static String Four = "4",
-			Horizontal = "Horizontal";
-}
